89b65618f4959dbe70d3b4cc1fbe14c88ae05cdd
Jay Shah <sonjay97@gmail.com>
2023-07-11 19:49:12 -0500

    [Update] Update to UI files

--- App.js
+++ App.js
@@ -1,14 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, } from 'react-native';
-import ProductsScreen from './src/screens/ProductsScreen';
-import ProductDetailsScreen from './src/screens/ProductDetailsScreen';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import ProductsScreen from "./src/screens/ProductsScreen";
+import ProductDetailsScreen from "./src/screens/ProductDetailsScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-
       {/* <ProductsScreen /> */}
-      <ProductDetailsScreen/>
+      <ProductDetailsScreen />
 
       <StatusBar style="auto" />
     </View>
@@ -18,7 +17,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    
+    backgroundColor: "#fff",
   },
 });
--- src/screens/ProductDetailsScreen.js
+++ src/screens/ProductDetailsScreen.js
@@ -1,37 +1,51 @@
-import { FlatList, StyleSheet, View, Image, useWindowDimensions, Text, ScrollView, Pressable } from 'react-native';
+import {
+  FlatList,
+  StyleSheet,
+  View,
+  Image,
+  useWindowDimensions,
+  Text,
+  ScrollView,
+  Pressable,
+} from "react-native";
 import products from "../data/products";
 
 const ProductDetailsScreen = () => {
   const product = products[0];
 
-  const {width} = useWindowDimensions();
+  const { width } = useWindowDimensions();
   return (
     <View>
-        <ScrollView>
-      {/* Image Carousel */}
-        <FlatList 
-            data={product.images}
-            renderItem={({ item }) => (
-                <Image source={{ uri: item}} style={{ width: width, aspectRatio: 1}} />
-            )}
-            horizontal
-            showsHorizontalScrollIndicator={false}
-            pagingEnabled
+      <ScrollView>
+        {/* Image Carousel */}
+        <FlatList
+          data={product.images}
+          renderItem={({ item }) => (
+            <Image
+              source={{ uri: item }}
+              style={{ width: width, aspectRatio: 1 }}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
         />
-        
-    <View style={{ padding: 20}}>
-      {/* Title */}
-        <Text style={styles.title}>{product.name}</Text>
 
-      {/* Price */}
-        <Text style={styles.price}>${product.price}</Text>
+        <View style={{ padding: 20 }}>
+          {/* Title */}
+          <Text style={styles.title}>{product.name}</Text>
 
-      {/* Description */}
-        <Text style={styles.description}>{product.description}</Text>
-    </View>
-    </ScrollView>
+          {/* Price */}
+          <Text style={styles.price}>${product.price}</Text>
+
+          {/* Description */}
+          <Text style={styles.description}>{product.description}</Text>
+        </View>
+      </ScrollView>
       {/* Add to cart button */}
-    
+      <Pressable style={styles.button}>
+        <Text style={styles.buttonText}>Add To Cart</Text>
+      </Pressable>
 
       {/* Navigation icon */}
     </View>
@@ -39,22 +53,37 @@ const ProductDetailsScreen = () => {
 };
 
 const styles = StyleSheet.create({
-    title: {
-        fontSize: 34,
-        fontWeight: "500",
-        marginVertical: 10,
-    },
-    price: {
-        fontWeight: "500",
-        fontSize: 16,
-        letterSpacing: 1.5,
-    },
-    description: {
-        marginVertical: 10,
-        fontSize: 18,
-        lineHeight: 30,
-        fontWeight: "300",
-    },
+  title: {
+    fontSize: 34,
+    fontWeight: "500",
+    marginVertical: 10,
+  },
+  price: {
+    fontWeight: "500",
+    fontSize: 16,
+    letterSpacing: 1.5,
+  },
+  description: {
+    marginVertical: 10,
+    fontSize: 18,
+    lineHeight: 30,
+    fontWeight: "300",
+  },
+  button: {
+    position: "absolute",
+    backgroundColor: "black",
+    bottom: 30,
+    width: "90%",
+    alignSelf: "center",
+    padding: 20,
+    borderRadius: 100,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "500",
+    fontSize: 16,
+  },
 });
-    
+
 export default ProductDetailsScreen;
