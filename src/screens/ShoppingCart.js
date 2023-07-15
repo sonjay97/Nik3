1243d769f788c49b1fde35db7308e2b7fa75e770
Jay <jsshah@ncsu.edu>
2023-07-14 19:21:43 -0500

    [Shopping Cart] Add Shopping Cart Screen

--- src/screens/ShoppingCart.js
+++ src/screens/ShoppingCart.js
@@ -1,37 +1,44 @@
-import { Text, FlatList, View, StyleSheet } from 'react-native';
+import { Text, FlatList, View, StyleSheet, Pressable } from 'react-native';
 import cart from '../data/cart';
 import CartListItem from '../components/CartListItem';
+
+const ShoppingCartTotals = () => (
+    <View style={styles.totalsContainer}>
+        <View style={styles.row}>
+            <Text style={styles.text}>Subtotal</Text>
+            <Text style={styles.text}>
+                410.00 US$
+            </Text>
+        </View>
+        <View style={styles.row}>
+            <Text style={styles.text}>Delivery</Text>
+            <Text style={styles.text}>
+                10.00 US$
+            </Text>
+        </View>
+        <View style={styles.row}>
+            <Text style={styles.textBold}>Total</Text>
+            <Text style={styles.text}>
+                420.00 US$
+            </Text>
+        </View>
+    </View>
+    
+)
 
 const ShoppingCart = () => {
 
     return (
-        <FlatList
-            data={cart} 
-            renderItem={({item}) => <CartListItem cartItem={item} />}
-            ListFooterComponent={() => (
-                <View style={styles.totalsContainer}>
-                    <View style={styles.row}>
-                        <Text style={styles.text}>Subtotal</Text>
-                        <Text style={styles.text}>
-                            410.00 US$
-                        </Text>
-                    </View>
-                    <View style={styles.row}>
-                        <Text style={styles.text}>Delivery</Text>
-                        <Text style={styles.text}>
-                            10.00 US$
-                        </Text>
-                    </View>
-                    <View style={styles.row}>
-                        <Text style={styles.textBold}>Total</Text>
-                        <Text style={styles.text}>
-                            420.00 US$
-                        </Text>
-                    </View>
-                </View>
-                
-            )}
-        />
+        <>
+            <FlatList
+                data={cart} 
+                renderItem={({item}) => <CartListItem cartItem={item} />}
+                ListFooterComponent={ShoppingCartTotals}
+            />
+            <Pressable style={styles.button}>
+                <Text style={styles.buttonText}>Checkout</Text>
+            </Pressable>
+      </>
     );
 };
 
@@ -56,8 +63,21 @@ const styles = StyleSheet.create({
         font: 16,
         fontWeight: '500',
     },
-
-
+    button: {
+        position: "absolute",
+        backgroundColor: "black",
+        bottom: 30,
+        width: "90%",
+        alignSelf: "center",
+        padding: 20,
+        borderRadius: 100,
+        alignItems: "center",
+      },
+      buttonText: {
+        color: "white",
+        fontWeight: "500",
+        fontSize: 16,
+      },
 })
 
 export default ShoppingCart;
